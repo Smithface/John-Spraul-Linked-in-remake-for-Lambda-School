132b549ab0ec3dfae9a3a5076697aea2b304f0e3
Smithface <kylewibblesmith@gmail.com>
2018-02-07 16:15:02 -0500

    basic NavBar layout complete

---
+++ linkedin/src/components/NavBar/NavBar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './navBar.css';
+
+class NavBar extends React.Component {
+  state = {
+
+  }
+
+  render() {
+    return(
+    <div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+    );
+  }
+}
+
+export default NavBar;
