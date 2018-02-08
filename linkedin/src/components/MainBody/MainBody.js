fdad30255d285c033f9f12ed02897b31a82fff99
Smithface <kylewibblesmith@gmail.com>
2018-02-07 22:56:37 -0500

    separated middle column with margin
    planning to work on left box next

--- linkedin/src/components/MainBody/MainBody.js
+++ linkedin/src/components/MainBody/MainBody.js
@@ -24,7 +24,7 @@ class MainBody extends React.Component {
 
     return(
       <div style={styleFlexBoxRow} className ="mainBody__container">
-        <div style={styleFlexBoxColumn} className="container__box-left">{/* Leftmost Div START */}
+        <div style={styleFlexBoxColumn} className="container__box container__box-grey container__box-left">{/* Leftmost Div START */}
           <div style={styleFlexBoxColumn}>
             <div>913</div>            {/* Number */}
             <div>Your connections</div>            {/* Your Connections */}
@@ -43,11 +43,11 @@ class MainBody extends React.Component {
         </div>{/* Leftmost DIV END */}
 
         <div style={styleFlexBoxColumn} className="container__box-middle">{/* Middle Div START */}
-          <div style={styleFlexBoxRow}>
+          <div style={styleFlexBoxRow} className="container__box-grey container__box-topMiddle">
             <div>no pending invitations</div>            {/* no pending invitations */}
             <div>manage all</div>            {/* manage all */}
           </div>
-          <div>
+          <div className="container__box-grey">
             <div>people you may know</div>            {/* People you may know */}
             <div style={styleFlexBoxRow}>            {/* insert a shit ton of people */}
               {dummyData.map((link, i) => {
@@ -57,7 +57,7 @@ class MainBody extends React.Component {
           </div>
         </div>{/* Middle Div END */}
 
-        <div style={styleFlexBoxColumn} className="container__box-right">{/* Right Div START */}
+        <div style={styleFlexBoxColumn} className="container__box-grey container__box-right">{/* Right Div START */}
           <div style={styleFlexBoxColumn}>
             <div style={styleFlexBoxRow}>Promoted</div>            {/* Promoted */}
             <div></div>            {/* insert random stuff */}
