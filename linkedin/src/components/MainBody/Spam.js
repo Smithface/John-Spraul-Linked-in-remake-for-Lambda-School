20a2d2134ea62206fffa668631b78030cba353f1
Smithface <kylewibblesmith@gmail.com>
2018-02-09 00:11:33 -0500

    added promoted spam items on right
    need to style everything properly still

--- linkedin/src/components/MainBody/Spam.js
+++ linkedin/src/components/MainBody/Spam.js
@@ -1,8 +1,39 @@
 import React from 'react';
 
 function Spam(props) {
+  const wholeBoxStyling = {
+    display: 'flex',
+    justifyContent: 'space-around',
+    flex: '1',
+  };
+
+  const imageStyling = {
+    height: '50px',
+    width: '50px',
+    borderRadius: '50%',
+  };
+
+  const titleStyling = {
+    fontWeight: 'bold',
+  };
+
+  const wordsStyling = {
+    fontSize: '.7em',
+  }
+
+  const arrowStyling = {
+    alignSelf: 'center',
+  };
+
   return (
-    null
+    <div style={wholeBoxStyling}>
+        <img style={imageStyling} src={props.spamInfo.pic} alt='somewhat broken' />
+        <div>
+          <div style={titleStyling}>{props.spamInfo.title}</div>
+          <div style={wordsStyling}>{props.spamInfo.words}</div>
+        </div>
+        <div style={arrowStyling}>&#x27A4;</div>
+    </div>
   );
 } 
 
--- linkedin/src/components/MainBody/dummyAds.js
+++ linkedin/src/components/MainBody/dummyAds.js
@@ -1,18 +1,18 @@
 const dummyAds = [
   {
-    picture: 'https://images.freshop.com/00037600141734/567dd04f0355fc86fcb7eedd7600de3d_medium.png',
-    title: '',
-    words: '',
+    pic: 'https://images.freshop.com/00037600141734/567dd04f0355fc86fcb7eedd7600de3d_medium.png',
+    title: 'Online Spam Degree',
+    words: 'Lorem spamsum dolor sit amet consectetur adipisicing elit. Vero, et in dolor blanditiis sunt modi.',
   },
   {
-    picture: 'https://images.freshop.com/00037600169004/c9a1f98c2393b39d5aa7bbf7ef10bc76_medium.png',
-    title: '',
-    words: '',
+    pic: 'https://images.freshop.com/00037600169004/c9a1f98c2393b39d5aa7bbf7ef10bc76_medium.png',
+    title: 'Highest-Ranked Spam in TX',
+    words: 'Lorem spamsum dolor sit amet consectetur, adipisicing elit. At porro ab repudiandae, odit.',
   },
   {
-    picture: 'https://images.freshop.com/00037600642057/73bbcab76b0dddd562371267f500bebc_medium.png',
-    title: '',
-    words: '',
+    pic: 'https://images.freshop.com/00037600642057/73bbcab76b0dddd562371267f500bebc_medium.png',
+    title: 'New Spam System ASPAMAP',
+    words: 'Lorem spamsom dolor sit amet consectetur adipisicing elit. Sed ad blanditiis nesciunt? Officiis quos possimus omnis.',
   },
 ]
 
